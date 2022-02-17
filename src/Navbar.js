49b67a2633a46aed5e3c12d259e72bb6e754c230
aniskhoja <anis.khojaa@gmail.com>
2022-02-16 18:34:04 -0800

    Navbar boilerplate

---
+++ src/Navbar.js
@@ -0,0 +1,52 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { FaBars, FaTwitter } from 'react-icons/fa'
+import { links, social } from './data'
+import logo from './logo.svg'
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksConatainerRef = useRef(null)
+  const linksRef = useRef(null)
+  
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height
+    if (showLinks) {
+      linksConatainerRef.current.style.height = `${linksHeight}px`
+    } else {
+      linksConatainerRef.current.style.height = '0px'
+    }
+  }, [showLinks])
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
+            <FaBars />
+          </button>
+        </div>
+
+        <div ref={linksConatainerRef} className='links-container'>
+          <ul ref={linksRef} className='links'>
+            {links.map(link => {
+              const { id, url, text } = link;
+              return (
+                <li key={id} ><a href={url}>{text}</a></li>
+              )
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {social.map(link => {
+            const { id, url, icon } = link;
+            return (
+              <li key={id}><a href={url}>{icon}</a></li>
+            )
+          })}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
